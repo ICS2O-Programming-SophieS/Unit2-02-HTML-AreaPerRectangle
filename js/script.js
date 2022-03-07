2f34db76e3e4ff9a236040b4f22e6b8c6f765b2e
Sophie Student <83235604+SophieStudent@users.noreply.github.com>
2022-03-07 01:22:10 +0000

    Added files to calculate area and perimeter of a rectangle

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Ms Raffin
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function calcAreaPer () {
+  // this function does basic math
+
+  document.getElementById('calc-area').innerHTML = 'The area is ' + (5*3) + 'cm²'
+	document.getElementById('calc-per').innerHTML = 'The perimeter is ' + (2*5 + 2*3) + 'cm'
+}
+
